7efb777e1559cfbb72a603d89f29e12aed01c505
Dewane Mutunga <dewane.mutunga@gmail.com>
2014-07-20 13:04:01 -0400

    Initial Commit

---
+++ easy-crm.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Plugin Name: Easy CRM
+ * Plugin URI: http://decodingwp.com/easy-crm
+ * Version: 1.0
+ * Author: Dewane Mutunga
+ * Author URI: http://dewanemutunga.com
+ * Description: A simple, easy-to-use CRM system for WordPress
+ * License: GPL2
+ * Text Domain: ecrm
+ */
+
+/**
+ * primary class for EasyCRM
+ *
+ * @since 1.0.0
+ */
+class EasyCRM {
+     
+    /**
+     * Constructor for EasyCRM class. Called when plugin is initialised
+     */
+     public function __construct() {
+		
+		// define plugin name
+		define( 'ECRM_NAME', 'EasyCRM' );
+		
+		// define plugin version
+		define( 'ECRM_VERSION', '1.0' );
+		
+		// define plugin directory
+		define( 'ECRM_DIR', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+		
+		// define plugin root file
+		define( 'ECRM_URL', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+
+		// load text domain
+		add_action( 'init', array( $this, 'load_textdomain' ) );
+		
+		// require additional plugin files
+		$this->includes();
+	}
+	
+    /**
+	 * require additional plugin files
+	 *
+	 * @since 1.0.0
+	 */
+	private function includes() {
+		require_once( ECRM_DIR . 'includes/admin/class-ecrm-cpt.php' );		// custom post type class
+	}
+     
+}
+$EasyCRM = new EasyCRM;
---
+++ includes/admin/class-ecrm-cpt.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * ECRM_CPT class
+ *
+ * This class is responsible for creating the new taxonomy and its
+ * corresponding options and settings.
+ *
+ * The taxonomy includes the typical Name, Slug, and Description fields
+ * but also adds a new field called "Post Listing Title" to the add/edit
+ * screens.
+ *
+ * Posts can be assigned to the taxonomy from both the manage posts
+ * screen and the edit post screens themselves. Posts can only be assigned
+ * to one term.
+ *
+ * @since 1.0.0
+ */
+ 
+ if ( ! defined( 'ABSPATH' ) ) exit; // no accessing this file directly
+ 
+ class ECRM_CPT {
+	 
+	 /**
+	 * constructor for ECRM_CPT class
+	 */
+	 public function __construct() {
+	 
+		 add_action( 'init', array( $this, 'register_custom_post_type' ) );
+	 }
+	 
+	 /**
+	 * Registers a Custom Post Type called contact
+	 */
+	public function register_custom_post_type() {
+	    register_post_type('contact', array(
+	        'labels' => array(
+	            'name'               => _x( 'Contacts', 'post type general name', 'ecrm' ),
+	            'singular_name'      => _x( 'Contact', 'post type singular name', 'ecrm' ),
+	            'menu_name'          => _x( 'Contacts', 'admin menu', 'ecrm' ),
+	            'name_admin_bar'     => _x( 'Contact', 'add new on admin bar', 'ecrm' ),
+	            'add_new'            => _x( 'Add New', 'contact', 'ecrm' ),
+	            'add_new_item'       => __( 'Add New Contact', 'ecrm' ),
+	            'new_item'           => __( 'New Contact', 'ecrm' ),
+	            'edit_item'          => __( 'Edit Contact', 'ecrm' ),
+	            'view_item'          => __( 'View Contact', 'ecrm' ),
+	            'all_items'          => __( 'All Contacts', 'ecrm' ),
+	            'search_items'       => __( 'Search Contacts', 'ecrm' ),
+	            'parent_item_colon'  => __( 'Parent Contacts:', 'ecrm' ),
+	            'not_found'          => __( 'No conttacts found.', 'ecrm' ),
+	            'not_found_in_trash' => __( 'No contacts found in Trash.', 'ecrm' ),
+	        ),
+	         
+	        // Frontend
+	        'has_archive'        => false,
+	        'public'             => false,
+	        'publicly_queryable' => false,
+	         
+	        // Admin
+	        'capability_type' => 'post',
+	        'menu_icon'     => 'dashicons-businessman',
+	        'menu_position' => 10,
+	        'query_var'     => true,
+	        'show_in_menu'  => true,
+	        'show_ui'       => true,
+	        'supports'      => array(
+	            'title',
+	            'author',
+	            'comments', 
+	        ),
+	    )); 
+	}
+ }
+ new ECRM_CPT();
